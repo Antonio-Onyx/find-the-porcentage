9c0286d0c5257fb2d38ae492c0400dc97e8db31c
Gerardo Macias <gerardo_macias317@ciencias.unam.mx>
2023-07-23 15:38:25 -0600

    ejercicio terminado

--- find_the_porcentage.py
+++ find_the_porcentage.py
@@ -1,3 +1,5 @@
+
+
 if __name__ == '__main__':
     n = int(input())
     student_marks = {}
@@ -7,4 +9,11 @@ if __name__ == '__main__':
         student_marks[name] = scores
     query_name = input()
     
+    #print(student_marks)
     
+    
+    for key in student_marks.items():
+        if key[0] == query_name:
+            porcentage = sum(key[1]) / len(key[1])
+            print("{0:.2f}".format(porcentage))
+            
